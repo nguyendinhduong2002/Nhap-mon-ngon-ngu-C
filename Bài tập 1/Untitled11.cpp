598cc32ff99dcc0288b3c1785a6a6a42eec91379
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2020-11-12 17:01:34 +0700

    Add files via upload

---
+++ Bài tập 1/Untitled11.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+  // khai bao bien a kieu so nguyen
+  int a;
+  // khao bao tra tri bien a
+  a = 4;
+  // khai bao va khoi tao bien
+  int b = 2;
+  // khai bao nhieu bien va khoi tao
+  int c = 1,d = 3;
+  // khai bao nhieu bien
+  int e, f, g, r;
+  printf("bien a = %d va bien b = %d",a,b);
+  e = a + b;
+  printf("\n");
+  printf("tong a + b = %d",e);
+  printf("\n");
+  printf("tong %d + %d = %d",a,b,e);
+  printf("\n");
+  r =a + b - c* d;
+  printf("\n");
+  printf("tong ((a + b)- c)* d = %d",r);
+  printf("tong ((%d + %d)- %d)* %d = %d",a,b,c,d,r);
+  printf("\n");
+  getchar();
+  return 0;
+}
