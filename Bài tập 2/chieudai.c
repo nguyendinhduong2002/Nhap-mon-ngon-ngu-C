a76df613cfcc1819a95e443ad29a7fc5eca7df09
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2020-12-10 09:26:24 +0700

    Create chieudai.c

---
+++ Bài tập 2/chieudai.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+    //khai bao bien
+    int l,Met,Xentimet,Milimet;
+    printf(" Nhap l= ");
+    scanf("%d", &l	);
+ 	Met=l;
+    Xentimet=l*100;
+    Milimet=l*1000;
+    printf("Met\tXentimet\tMilimet\n%d\t%d\t\t%d",Met,Xentimet,Milimet);
+    return 0;
+}
