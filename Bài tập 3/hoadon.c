b3ab04ef1725c71874705d781e92eaf016a0820d
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2020-12-10 16:53:20 +0700

    Add files via upload

---
+++ Bài tập 3/hoadon.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+	// khai bao bien
+	char a;
+	long b;
+  int dongia,soluong,thanhtien;
+  
+  printf ("hay nhap don gia  = "); scanf("%d",&a);
+    
+  printf ("hay nhap so luong =  "); scanf ("%d",&b);
+  
+  if (soluong <5){
+  thanhtien = a * b;
+  printf("thanh tien \n");
+  printf("%d",thanhtien);
+  }
+  
+  else if (soluong>=5){
+  	//giam gia 25% = 75/100
+  	thanhtien = (a*b)*75/100;
+  	printf("thanh tien \n");
+  	printf ("%d",thanhtien );
+  }
+  
+  getchar ();
+  return 0;
+}
