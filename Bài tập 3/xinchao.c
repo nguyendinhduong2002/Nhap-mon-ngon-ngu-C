5ce0313576c824d6de04e96c4dff0db124c0d554
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2020-12-24 14:10:31 +0700

    Update xinchao.c

--- Bài tập 3/xinchao.c
+++ Bài tập 3/xinchao.c
@@ -1,10 +1,8 @@
-#include <iostream> 
- using namespace std; 
- int main () 
- { 
+#include <stdio.h> 
+ 
+ int main ()  { 
 	int i;
-	for(i= 0; i < 10; i++)
-	{
-		printf("xin chao\n");
+	for(i= 0; i < 10; i++){
+	printf("xin chao\n");
  } 
  }
