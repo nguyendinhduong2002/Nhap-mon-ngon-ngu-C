371aaa88532be26955f4a171bbb8bfd9593e1b3d
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2020-12-28 17:49:09 +0700

    Update and rename Bài tập 3/tong10.c to 3./tong10.c

---
+++ 3./tong10.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main() {    
+    int sum;
+    int count;
+    for(count = 1 ; count <= 10;count++){
+    	sum = sum + count;
+	}
+	printf("tong tu mot den muoi = %d ", sum);
+    return 0;    	 
+}
+
+
+
+
+#bao  gồm <stdio.h> 
+
+int chính(){
+  
+int tong = 0 (int tong = 0);
+
+for (int dem = 1; dem <= 10; dem++)
+tong = tông + dem;
+  {
+    printf("tong la:%d ", tông);
+  }
+ người getchar();
+  trả lại 0;
+}
