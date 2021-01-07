c62c5b92e4189c4a1fb3d161fe22d2223a85e544
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2021-01-07 16:40:03 +0700

    Update kt01.c

--- Bài tập 3/kt01.c
+++ Bài tập 3/kt01.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
-
-int main() 
-{
-   int a;
-   printf("bang cuu chuong \n\n\n");
-   printf("bang 2:\n 2x1= 2\n 2x2 =4\n 2x3 =6\n\n");
-   printf("bang 3:\n 3x1= 3\n 3x2 =6\n 3x3 =9\n\n");
-   printf("bang 4:\n 4x1= 4\n 4x2 =8\n 4x3 =12\n\n");
-
-return 0;
-}
+#include<stdio.h>
+int main () {
+	
+	printf("Bang cuu chuong:");
+	int n;
+	n = 2;
+	printf("bang cuu chuong 2\n");
+	printf(" 2*1 =%d\n 2*2 = %d\n 2*3 =%d\n",n*1,n*2,n*3);
+	n = 3;
+	printf("bang cuu chuong 3\n");
+	printf(" 3*1 =%d\n 3*2 = %d\n 3*3 =%d\n",n*1,n*2,n*3);
+	n = 4;
+	printf("bang cuu chuong 4\n");
+	printf(" 4*1 =%d\n 4*2 = %d\n 4*3 =%d\n",n*1,n*2,n*3);
+	n = 5;
+	printf("bang cuu chuong 5\n");
+	printf(" 5*1 =%d\n 5*2 = %d\n 5*3 =%d\n",n*1,n*2,n*3);
+	
+	return 0;
+	}
