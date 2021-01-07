40459c9f18dee3ba45c739aae71e11a51d2b01a0
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2021-01-07 16:35:39 +0700

    Update kt01.c

--- Bài tập 3/kt01.c
+++ Bài tập 3/kt01.c
@@ -1,26 +1,12 @@
-  
 #include <stdio.h>
 
-int main()
-int a, b, c;
-  a = 2, b = 3, c = 4;
-  //hien thi ra man hinh BAN CUU CHUONG
-  printf("\t                         BANG CUU CHUONG\n");
-  //hien thi BAN CUU CHUONG 2 3 4 , 3 dong dau
-  printf("BANG CUU CHUONG 2\n");
-  printf("%d x 1 = %d\n", a, a * 1);
-  printf("%d x 2 = %d\n", a, a * 2);
-  printf("%d x 3 = %d\n", a, a * 3);
+int main() 
+{
+   int a;
+   printf("bang cuu chuong \n\n\n");
+   printf("bang 2:\n 2x1= 2\n 2x2 =4\n 2x3 =6\n\n");
+   printf("bang 3:\n 3x1= 3\n 3x2 =6\n 3x3 =9\n\n");
+   printf("bang 4:\n 4x1= 4\n 4x2 =8\n 4x3 =12\n\n");
 
-  printf("BANG CUU CHUONG 3\n");
-  printf("%d x 1 = %d\n", b, b * 1);
-  printf("%d x 2 = %d\n", b, b * 2);
-  printf("%d x 3 = %d\n", b, b * 3);
-
-  printf("BANG CUU CHUONG 4\n");
-  printf("%d x 1 = %d\n", c, c * 1);
-  printf("%d x 2 = %d\n", c, c * 2);
-  printf("%d x 3 = %d\n", c, c * 3);
-
-    return 0; 
+return 0;
 }
