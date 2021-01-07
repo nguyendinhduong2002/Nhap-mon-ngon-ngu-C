3d01a13d18ff44f0eaf49648fe834079a1f0e399
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2021-01-07 17:04:30 +0700

    Update kt01.c

--- Bài tập 3/kt01.c
+++ Bài tập 3/kt01.c
@@ -18,3 +18,39 @@ int main () {
 	
 	return 0;
 	}
+
+
+
+
+bang cuuc chuong ngắn gọn
+
+include <stdio.h>
+
+int main(){
+	int a,b;
+	printf("Bang 2\n");
+	a=2;
+	for (b=1;b<=3;b++){
+		printf("%d x %d = %d\n",a,b,a*b);
+	}
+	
+	printf("Bang 3\n");
+	a=3;
+	for (b=1;b<=3;b++){
+		printf("%d x %d = %d\n",a,b,a*b);
+	}
+	
+	printf("Bang 4\n");
+	a=4;
+	for (b=1;b<=3;b++){
+		printf("%d x %d = %d\n",a,b,a*b);
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
