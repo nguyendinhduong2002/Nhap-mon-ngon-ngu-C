3b36d3e6d9c6fb171f731ef91a20805fe9718e5c
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2021-01-08 09:45:05 +0700

    Create kt04.c

---
+++ Bài tập 3/kt04.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+ int n,a,s=0;
+	printf("nhap so nguyen > 0: "); 
+  	scanf("%d",&n);
+	a =n%10; 
+	s+=a; n/= 10;
+	a= n%10; 
+	s+=a; n/= 10;
+	a= n%10;
+	s+=a;
+	printf("%d",s);
+return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+    int n,s=0;
+    printf("Nhap so nguyen > 0: ");
+    scanf("%d",&n);
+    for(;n!=0;){
+        s=s+ n%10;
+        n/= 10;
+    }    
+    printf("%d",s);
+     return 0;
+}
