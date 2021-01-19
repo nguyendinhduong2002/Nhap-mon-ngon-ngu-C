3e5c01af9e3c8bdab99ea118899fc68f3a2eafee
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2021-01-19 16:43:00 +0700

    Update mang_03.c

--- Bài tập 4/mang_03.c
+++ Bài tập 4/mang_03.c
@@ -1,19 +1,19 @@
-  
+   
 #include <stdio.h>
 
-int main(void) {
+int main() {
 	int n;
-	printf("Nhap so luong phan tu: ");scanf
+	printf("Nhap gia tri phan tu : ");scanf
 	("%d",&n);
 		int a[n];	
 	for (int b=0;b<n;b++){
-	printf("Nhap phan tu thu %d: ",b+1);scanf
+	printf("Nhap phan tu thu la %d: ",b+1);scanf
 		("%d",&a[b]);
-	}	
+}	
 	printf("\n\nMang a[");
 	for (int b=0;b<n;b++){
 		printf("%d ",a[b]);
-	}
+}
 	printf("]\n\n");
 		int count_odd=0,count_even=0,count_0=0;
 	for (int b=0; b<n; b++){
@@ -26,19 +26,18 @@ int main(void) {
 		else if (a[b]%2 == 0){
 			count_even+=1;
 		}
-	}
+}
 	printf("Mang co %d so le, %d so chan, %d so 0\n",count_odd,count_even,count_0);	
 	printf("\nCac so le: ");
 	for(int b=0;b<n;b++){
 		if (a[b]%2 != 0){
 			printf("%d  ",a[b]);
 		}
-	}	
+}	
 	printf("\nCac so chan: ");
 	for(int b=0;b<n;b++){
 		if (a[b]%2 == 0 && a[b]!=0){
 			printf("%d  ",a[b]);
-		}
-	}
-
+}
+}
 }
