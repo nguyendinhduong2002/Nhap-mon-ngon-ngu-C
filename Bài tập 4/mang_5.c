36e27e1c6e656422aa0c6b27ccc7c5e8ddcd8ad6
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2021-01-21 16:55:05 +0700

    Update mang_5.c

--- Bài tập 4/mang_5.c
+++ Bài tập 4/mang_5.c
@@ -3,22 +3,23 @@ int main() {
   int i;
   int n;
   int m; 
-    printf("nhap thu tu mang =");
+    printf("nhap n =");
     scanf("%d",&n);
  int a[n];
  for( i=0;i<n;i++){
-    printf("nhap phan  %d =",i+1);
+    printf("nhap phan tu  %d =",i+1);
     scanf("%d",&a[i]);
- 	}
+ }
     printf("cac phan tu cua mang a :");
  for(int i=0;i<n;i++){
     printf("%d ",a[i]);
- 	}   
+ }   
 	printf("\n Nhap so m can kiem tra: ");scanf("%d",&m); 
 	for (i=0; i<n; i++){
 		if (a[i]==m){
-		break;
-	}	
+			
+			break;
+		}	
 	}	
 	if (i< 0){
 		printf("ton tai");
