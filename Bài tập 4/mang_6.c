56140e7b78787bfea49f4550a098f5dedb50ae3e
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2021-01-21 16:57:50 +0700

    Create mang_6.c

---
+++ Bài tập 4/mang_6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+  int i;
+  int n;
+  int m; 
+    printf("nhap n =");
+    scanf("%d",&n);
+ int a[n];
+ for( i=0;i<n;i++){
+    printf("nhap phan tu   %d =",i+1);
+    scanf("%d",&a[i]);
+ }
+    printf("cac phan tu cua mang a :");
+ for(int i=0;i<n;i++){
+    printf("%d ",a[i]);
+ }   
+	printf("\n Nhap so m can kiem tra: ");scanf("%d",&m); 
+	for (i=0; i<n; i++){
+		if (a[i]==m){
+			
+			break;
+		}	
+	}	
+	if (i< 0){
+		printf("ton tai");
+	}
+	else{
+		printf("Khong ton tai");
+	}	
+}
