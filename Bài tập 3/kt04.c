bcbc32f3db843aba25e4bafd299b263972e04414
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2021-01-24 15:41:06 +0700

    Update kt04.c

--- Bài tập 3/kt04.c
+++ Bài tập 3/kt04.c
@@ -26,4 +26,18 @@ int main(){
     }    
     printf("%d",s);
      return 0;
+
 }
+#include <stdio.h>
+int main(){
+    int n;
+    int arata;
+    int s = 0;
+    printf("Nhap n: ");
+    scanf("%d",&n);
+    for(;n!=0;){
+        arata = n % 10;
+        s += arata;
+        n /= 10; 
+    printf("%d",s);}
+
