1a14cbfe0b07368221d45606d8b310677ae95d87
Nguyễn Đình Dương <72543260+nguyendinhduong2002@users.noreply.github.com>
2021-01-24 15:40:24 +0700

    Create KT_03.c

---
+++ Bài tập 3/KT_03.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main(){
+int i;
+while(i<20){
+  i++;
+if (i%2 !=0){
+if(i == 5){  
+continue;}
+if(i == 11){
+continue;}
+if (i == 15 ){
+continue;}
+printf("%d\t", i);
+
+}}
+	getchar();
+	return 0;
+}
